83e458db35eeecb5ea19c42559ca4f9b1f404c37
Dee Bryant <tolkadot@gmail.com>
2015-08-02 20:13:29 +1000

    Update sumOfARangeCH04.js

--- sumOfARangeCH04.js
+++ sumOfARangeCH04.js
@@ -1,16 +1,18 @@
 function range(start, end) {
 var arr= [];
-  for(var i = start; i<end; i++){
+  for(var i = start; i<=end; i++){
    arr.push(i);}
   //console.log(arr)
 return arr;
 }
 
 function sum(numbers){
+  var total = 0;
+  for(var j=0; j<numbers.length; j++){
+  total += numbers[j];
+  }
+  return total;
 }
 
 console.log(range(1, 10));
-//console.log(sum(range(1, 10)));
-// → 55
-//console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
