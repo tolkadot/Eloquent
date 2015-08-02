1a8ee55f4a05b74b6b9aba93a396ee3b4005dccd
Dee Bryant <tolkadot@gmail.com>
2015-08-02 20:22:43 +1000

    Update sumOfARangeCH04.js

--- sumOfARangeCH04.js
+++ sumOfARangeCH04.js
@@ -1,7 +1,17 @@
-function range(start, end) {
+function range(start, end, step) {
 var arr= [];
+  if(step === undefined){
   for(var i = start; i<=end; i++){
    arr.push(i);}
+  }
+  else if(step > 0){
+  for(var i = start; i<=end; i+=step){
+   arr.push(i);}
+  }
+  else{
+  for(var i = start; i<=end; i+=step){
+   arr.push(i);}
+  }
   //console.log(arr)
 return arr;
 }
@@ -13,6 +23,7 @@ function sum(numbers){
   }
   return total;
 }
-
 console.log(range(1, 10));
 console.log(sum(range(1, 10)));
+console.log(range(1, 10, 2));
+//console.log(range(5, 2, -1));
