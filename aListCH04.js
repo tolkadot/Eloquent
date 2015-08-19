7152994f3e8ef5df26ae8afe329235cfc63521a0
Dee Bryant <tolkadot@gmail.com>
2015-08-19 20:09:16 +1000

    Update aListCH04.js

--- aListCH04.js
+++ aListCH04.js
@@ -14,14 +14,26 @@
 
 
 
+function arrayToList(arr) {
+    
+    for(i=arr.length-1; i>=0; i--)
+    var list = {
+  value: arr[i],
+  rest:  list };
+  console.log( list, list.rest);
+  }
+
+    
+
+
 
 // Your code here.
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
+//console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
+//console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
+//console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
