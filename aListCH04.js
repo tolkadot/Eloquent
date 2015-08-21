77f48547c325898f6da82dff39dc2908e14c48a9
Dee Bryant <tolkadot@gmail.com>
2015-08-21 11:36:45 +1000

    Update aListCH04.js

--- aListCH04.js
+++ aListCH04.js
@@ -33,7 +33,17 @@ function arrayToList(arr) {
     return arr;
 } 
 
+function prepend(element, list) {
+    newlist = null;
+    //console.log(element, list);
+    newlist = { value : element, rest: newlist};
+    
+    console.log(newlist);
+    
+}
 
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
 
 // Your code here.
 
